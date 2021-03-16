816c0298797b8de4e6b53e5e760e748ddf2224d8
pamykcharyyeva <pamyk.ch@gmail.com>
2021-03-16 02:06:39 -0400

    next step push to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_03_15_200911) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
