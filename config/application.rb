cc9f47c500e2c38b0063a178e8b3f0111df6d8b5
pamykcharyyeva <pamyk.ch@gmail.com>
2021-03-16 03:24:13 -0400

    change

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 require "view_component/engine"
-require "rails_12factor"
+include ActiveSupport::LoggerSilence if defined?(ActiveSupport::LoggerSilence)
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
