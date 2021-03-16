66734a37af769e180bfa32ca1d25ba5105c6b393
pamykcharyyeva <pamyk.ch@gmail.com>
2021-03-16 16:29:20 -0400

    logger

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-require "view_component/engine"
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,27 +18,5 @@ module SmallPm
     # the framework and any gems in your application.
   end
 
-  module RailsStdoutLogging
-    class StdoutLogger < defined?(::ActiveSupport::Logger) ? ::ActiveSupport::Logger : ::Logger
-      include ::LoggerSilence if defined?(::LoggerSilence)
-    end
-  
-    class Rails
-      def self.heroku_stdout_logger
-        logger       = StdoutLogger.new(STDOUT)
-        logger       = ActiveSupport::TaggedLogging.new(logger) if defined?(ActiveSupport::TaggedLogging)
-        logger.level = StdoutLogger.const_get(log_level)
-        logger
-      end
-  
-      def self.log_level
-        ([(ENV['LOG_LEVEL'] || ::Rails.application.config.log_level).to_s.upcase, "INFO"] & %w[DEBUG INFO WARN ERROR FATAL UNKNOWN]).compact.first
-      end
-  
-      def self.set_logger
-        STDOUT.sync = true
-      end
-    end
-  end
   
 end
