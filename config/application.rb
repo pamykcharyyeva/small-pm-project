6284cbe5cd34a8a14c4a1e3deba5fd09e8c491b2
pamykcharyyeva <pamyk.ch@gmail.com>
2021-03-16 16:04:47 -0400

    logger

--- config/application.rb
+++ config/application.rb
@@ -17,4 +17,28 @@ module SmallPm
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+
+  module RailsStdoutLogging
+    class StdoutLogger < defined?(::ActiveSupport::Logger) ? ::ActiveSupport::Logger : ::Logger
+      include ::LoggerSilence if defined?(::LoggerSilence)
+    end
+  
+    class Rails
+      def self.heroku_stdout_logger
+        logger       = StdoutLogger.new(STDOUT)
+        logger       = ActiveSupport::TaggedLogging.new(logger) if defined?(ActiveSupport::TaggedLogging)
+        logger.level = StdoutLogger.const_get(log_level)
+        logger
+      end
+  
+      def self.log_level
+        ([(ENV['LOG_LEVEL'] || ::Rails.application.config.log_level).to_s.upcase, "INFO"] & %w[DEBUG INFO WARN ERROR FATAL UNKNOWN]).compact.first
+      end
+  
+      def self.set_logger
+        STDOUT.sync = true
+      end
+    end
+  end
+  
 end
