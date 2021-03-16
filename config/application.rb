02da8b9e668e10d30ca20127ae82293dc3667100
pamykcharyyeva <pamyk.ch@gmail.com>
2021-03-16 03:12:50 -0400

    change

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require "view_component/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
 
 module SmallPm
   class Application < Rails::Application
