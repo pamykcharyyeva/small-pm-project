02360e3d4272e8609fed4f3c8ddb38c003d1b89f
pamykcharyyeva <pamyk.ch@gmail.com>
2021-03-17 01:17:03 -0400

    change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
       expire_after: 1.year,
       ttl: 1.year,
       key_prefix: "app:session:",
-      url: ENV.fetch("HEROKU_REDIS_GRAY_TLS_UR")
+      url: ENV.fetch("HEROKU_REDIS_GRAY_TLS_URL")
     }
   }
 
